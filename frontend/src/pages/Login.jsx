89255068cbdef0745939df707151f05bde7366b7
Wajahath zaman <mohammadwajahathzaman@gmail.com>
2025-07-14 05:45:30 +0530

    The login page had hardcoded path to the frontend. (rectified)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:4000/api/auth/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- frontend/src/utilities/api.js
+++ frontend/src/utilities/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // Use Vite environment variable
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-console.log("Resolved BaseURL:", baseURL);
 
 const api = axios.create({
   baseURL,
